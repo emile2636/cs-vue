7decdc7d4148fba30e43177a3c3de19204842800
emile2636 <16331731+emile2636@users.noreply.github.com>
2020-11-04 04:13:54 +0800

    tmp tmp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <CTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
+import CTable from "./views/CTable";
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { CTable },
 };
 </script>
 
@@ -22,7 +18,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
